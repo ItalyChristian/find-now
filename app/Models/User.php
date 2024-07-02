a9278451c4d05db40e6631bdfc4a0bbf26aabc05
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-01 21:10:20 -0300

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,6 +12,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    protected $guarded = [];
     /**
      * The attributes that are mass assignable.
      *
@@ -43,5 +45,21 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function announcements()
+    {
+        return $this->hasMany(Announcement::class);
+    }
+    public function image(): MorphMany
+    {
+
+        return $this->morphMany(Image::class, 'images');
+    }
+
+    public function address(): MorphMany
+    {
+
+        return $this->morphMany(Address::class, 'address');
     }
 }
